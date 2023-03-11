91ba762a899f2ade14f6588090a98fd676015b6f
Sami Ur Rehman Saqib <samisaqib10@gmail.com>
2023-03-11 14:36:34 -0500

    wikioeddia link function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-
 var userInput = document.getElementById("search");
-var pokeImg = document.getElementsByClassName("poke-pic");
-var pokeName = document.getElementsByClassName("pokemon-name");
-var pokeNum = document.getElementsByClassName("pokemon-number");
-var pokeTypeOne = document.getElementsByClassName("pokemon-type-1");
-var PokeTypeTwo = document.getElementsByClassName("pokemon-type-2");
+var pokeImg = document.getElementById("poke-pic");
+var pokeName = document.getElementById("pokemon-name");
+var pokeNum = document.getElementById("pokemon-number");
+var pokeTypeOne = document.getElementById("pokemon-type-1");
+var PokeTypeTwo = document.getElementById("pokemon-type-2");
 
 function getPokemon() {
   var requestUrl =
@@ -15,45 +14,59 @@ function getPokemon() {
     })
     .then(function (data) {
       console.log(data);
+      wikipedia(data);
       displayPokemonInfo(data);
+   
     });
 }
+function wikipedia(data){
+  console.log(data['name'])
+  var url = "https://en.wikipedia.org/w/api.php"; 
+var pokemon=data["name"];
+var params = {
+  action: "opensearch",
+  search: pokemon + " (pokemon)",
+  limit: "1",
+  namespace: "0",
+  format: "json"
+};
 
+url = url + "?origin=*";
+Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+fetch(url)
+  .then(function(response){return response.json();})
+  .then(function(response) {console.log(response);})
+  .catch(function(error){console.log(error);});
+
+}
 userInput.addEventListener("keypress", function (event) {
-  
   if (event.key === "Enter") {
     event.preventDefault();
     getPokemon();
-    for (var i = 0 ; i < userInput.length; i++)
-    userInput[i].value = "";
+    userInput.value = "";
+    // wikipedia();
   }
 });
 
 function displayPokemonInfo(data) {
-  for (var i = 0 ; i < pokeImg.length; i++)
-  pokeImg[i].src = data.sprites.front_default;
-  for (var i = 0 ; i < pokeName.length; i++)
-  pokeName[i].textContent = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-  for (var i = 0 ; i < pokeNum.length; i++)
-  pokeNum[i].textContent = "#" + data.id;
-  
-  
+  pokeImg.src = data.sprites.front_default;
+  pokeName.textContent = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+  pokeNum.textContent = "#" + data.id;
   var pokemonType1 =
     data.types[0].type.name.charAt(0).toUpperCase() +
     data.types[0].type.name.slice(1);
-    for (var i = 0 ; i < pokeTypeOne.length; i++)
-  pokeTypeOne[i].textContent = pokemonType1;
+  pokeTypeOne.textContent = pokemonType1;
   if (data.types[1]) {
     var pokemonType2 =
       data.types[1].type.name.charAt(0).toUpperCase() +
       data.types[1].type.name.slice(1);
-      for (var i = 0 ; i < PokeTypeTwo.length; i++)
-    PokeTypeTwo[i].textContent = pokemonType2;
+    PokeTypeTwo.textContent = pokemonType2;
   } else {
-    for (var i = 0 ; i < PokeTypeTwo.length; i++)
-    PokeTypeTwo[i].textContent = "-";
+    PokeTypeTwo.textContent = " ";
   }
- 
+
   // if()
+  
 }
 
